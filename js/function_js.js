f982a69ec61105a7bf1b9bc953ec024fabe10a8c
helper <vovan.danchik@mail.ru>
2018-10-28 01:12:51 +0600

    Add files via upload

--- js/function_js.js
+++ js/function_js.js
@@ -41,8 +41,7 @@ $(document).ready(function(){
     $("#show").bind("click", showMenu);
     
     $(".part1").bind("click", bodyColor);
-    
-    $(".part3").bind("click", calculate);
+
     $(".part4").bind("click", function(){
         alert("ТЫ ЧО ДУРАК!? ОН КОЛЬЯН КУРИТ! ЕМУ НЕКОГДА С НАМИ ТУСИТЬ!")
     });
@@ -51,32 +50,55 @@ $(document).ready(function(){
         $('#muztext').slideDown(1000);
     });
     
-    $(".part6").bind("click", function(){
+    $(".part3").bind("click", function(){
         $(".cols.col-8").css("background", "white").css("color", "black");
         $(".engineSys").slideDown(600);
     });
+    
+    $("#inBut").bind("click", function(){
+        
+        var men = $(".a").val();
+       var wom = $(".b").val();
+       var mon = $(".c").val();
+       var hom = $(".d").val();
+        var homd = $(".dd").val();
+       var eat = $(".e").val();
+        var eatP = $(".ee").val();
+       var dri1 = $(".f").val();
+        var dri2 = $(".ff").val();
+        var dri3 = $(".fff").val();
+       var dri21 = $(".g").val();
+        var dri22 = $(".g").val();
+        var dri23 = $(".g").val();
+       var dri31 = $(".h").val();
+        var dri32 = $(".h").val();
+        var dri33 = $(".h").val();
+        
+        var people = men + wom;
+        var summ = people * mon;
+        var home = hom * homd;
+        var eatAll = eat * eatP;
+        var drink1 = dri1 * dri2;
+        var drink2 = dri22 * dri23;
+        var drink3 = dri32 * dri33;
+        var drink = drink1+drink2;
+
+          alert("Количество участников " + people + "\n" 
+                +"Общий фонд "+ summ+"\n"
+                +"Жильё обойдётся в "+ home +"\n"
+                +"Хавчика на "+ eatAll +"\n"
+                +"Сушняка на "+ drink3 +"\n"
+                +"Бухла на "+ drink);
+        
+       $(".cols.col-7").show(500);
+        
+    });
+    
+    $("#outBut").bind("click", function(){
+        $(".cols.col-7").hide(500);
+    });
 });
 
-function calculate(){
-    var number = prompt("Количество участников торжества");
-    var summ = number * prompt("Взнос каждого участника");
-    if(!summ || summ<3000){
-        alert("ЭТО НЕ ДЕНЬГИ");
-     }else{
-    
-        if(summ>60000){
-            alert("БЮДЖЕТ СОСТАВЛЯЕТ " + summ+ "\n\n" + "СОЛИДНАЯ СУММА!");
-        }else alert("БЮДЖЕТ СОСТАВЛЯЕТ " + summ+ "\n\n" + "НА СОК И ЧИПСЫ ХВАТИТ!");
-    
-        var house = prompt("ЦЕНА ЖИЛЬЯ");
-    
-        if(summ<house){ 
-            alert("ВЫ СТОЛЬКО ДАЖЕ НЕ СОБРАЛИ! ТАК ЧТО С ЛЁХОЙ КЛЕНИНЫМ ОТПРАЗНУЕТЕ!");
-        }else 
-            alert("МОЖНО И БАБ ПОЗВАТЬ");
-   
-     }
-}
 
 
 
