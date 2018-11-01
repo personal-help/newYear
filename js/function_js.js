0ac70dabf836169c87d9e29eae1b4ae6a2c1f918
helper <vovan.danchik@mail.ru>
2018-11-01 23:07:38 +0600

    Add files via upload

--- js/function_js.js
+++ js/function_js.js
@@ -1,5 +1,11 @@
 function print(text)  { document.write(text); }
-function println(text){ document.write("<br>" + text); }   
+function println(text){ document.write("<br>" + text); }  
+
+var people, summ, home, eatAll, drink1, drink2, drink3, drink;
+
+var men,wom,mon,hom,homd,eat,eatP,dri1,dri2,dri3,dri21,dri22,dri23,dri31,dri32,dri33;
+
+        
 
 function bodyColor(){
     var col = prompt("Ввод цвета body по кодировке #0000 или английским названием!");
@@ -67,38 +73,51 @@ $(document).ready(function(){
     
     $("#inBut").bind("click", function(){
         
-        var men = $(".a").val();
-       var wom = $(".b").val();
-       var mon = $(".c").val();
-       var hom = $(".d").val();
-        var homd = $(".dd").val();
-       var eat = $(".e").val();
-        var eatP = $(".ee").val();
-       var dri1 = $(".f").val();
-        var dri2 = $(".ff").val();
-        var dri3 = $(".fff").val();
-       var dri21 = $(".g").val();
-        var dri22 = $(".g").val();
-        var dri23 = $(".g").val();
-       var dri31 = $(".h").val();
-        var dri32 = $(".h").val();
-        var dri33 = $(".h").val();
+        /*men = document.getElementById('a').innerHTML;
+        wom = document.getElementById('b').innerHTML;
+        mon = document.getElementById('c').innerHTML;
+        hom = document.getElementById('d').innerHTML;
+         homd = document.getElementById('dd').innerHTML;
+        eat = document.getElementById('e').innerHTML;
+         eatP = document.getElementById('ee').innerHTML;
+        dri1 = document.getElementById('f').innerHTML;
+         dri2 = document.getElementById('ff').innerHTML;
+         dri3 = document.getElementById('fff').innerHTML;
+        dri21 = document.getElementById('g').innerHTML;
+         dri22 =document.getElementById('gg').innerHTML;
+         dri23 = document.getElementById('ggg').innerHTML;
+        dri31 = document.getElementById('h').innerHTML;
+         dri32 = document.getElementById('hh').innerHTML;
+         dri33 = document.getElementById('hhh').innerHTML;*/
         
-        var people = men + wom;
-        var summ = people * mon;
-        var home = hom * homd;
-        var eatAll = eat * eatP;
-        var drink1 = dri1 * dri2;
-        var drink2 = dri22 * dri23;
-        var drink3 = dri32 * dri33;
-        var drink = drink1+drink2;
-
-          /*alert("Количество участников " + people + "\n" 
-                +"Общий фонд "+ summ+"\n"
-                +"Жильё обойдётся в "+ home +"\n"
-                +"Хавчика на "+ eatAll +"\n"
-                +"Сушняка на "+ drink3 +"\n"
-                +"Бухла на "+ drink);*/
+        
+        men = Number.parseInt($("#a").val());
+        wom = Number.parseInt($("#b").val());
+        mon = Number.parseInt($("#c").val());
+        hom = Number.parseInt($("#d").val());
+        homd = Number.parseInt($("#dd").val());
+        eat = Number.parseInt($("#e").val());
+        eatP = Number.parseInt($("#ee").val());
+        dri1 = Number.parseInt($("#f").val());
+        dri2 = Number.parseInt($("#ff").val());
+        dri3 = Number.parseInt($("#fff").val());
+        dri21 = Number.parseInt($("#g").val());
+        dri22 = Number.parseInt($("#gg").val());
+        dri23 = Number.parseInt($("#ggg").val());
+        dri31 = Number.parseInt($("#h").val());
+        dri32 = Number.parseInt($("#hh").val());
+        dri33 = Number.parseInt($("#hhh").val());
+        
+         people =  men +  wom;
+         summ = people * mon;
+         home = hom * homd;
+         eatAll = eat * eatP;
+         drink1 = dri1 * dri2;
+         drink2 = dri22 * dri23;
+         drink3 = dri32 * dri33;
+         drink = drink1+drink2;
+        
+        alert(people);
         
        $(".cols.col-7").show(500);
         
